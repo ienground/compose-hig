9b7220deb78e99d10696324e1601248dbeb3a5f0
IENGROUND <ericano.rhee@gmail.com>
2026-03-14 13:06:31 +0900

    ✨feat(adaptive): AdaptiveComponent 추가 및 종속성 버전 업데이트
    - 테마에 따라 Material과 Cupertino 컴포넌트를 전환하는 `adaptiveComponent` 추가
    - `lib-version-name`을 `0.1.1-alpha15`로 업데이트
    - `activity-compose`, `lifecycle`, `koin` 등 주요 라이브러리 버전 업데이트

---
+++ hig-adaptive/src/commonMain/kotlin/zone/ien/hig/adaptive/AdaptiveComponent.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2023-2024. Compose Cupertino project and open source contributors.
+ * Copyright (c) 2025. Scott Lanoue.
+ * Copyright (c) 2026. IENGROUND of IENLAB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+package zone.ien.hig.adaptive
+
+import androidx.compose.runtime.Composable
+
+@Composable
+@ExperimentalAdaptiveApi
+fun <T> adaptiveComponent(
+    material: @Composable () -> T,
+    cupertino: @Composable () -> T,
+): T {
+    return when (LocalTheme.current) {
+        Theme.Cupertino -> cupertino()
+        else -> material()
+    }
+}
+
+@Composable
+@ExperimentalAdaptiveApi
+fun <C, M, T> adaptiveComponent(
+    adaptation: Adaptation<C, M>,
+    material: @Composable (M) -> T,
+    cupertino: @Composable (C) -> T,
+    adaptationScope: AdaptationScope<C, M>.() -> Unit,
+): T {
+    adaptation.adaptationScope()
+
+    return when (LocalTheme.current) {
+        Theme.Cupertino -> cupertino(adaptation.rememberUpdatedCupertinoAdaptation())
+        else -> material(adaptation.rememberUpdatedMaterialAdaptation())
+    }
+}
